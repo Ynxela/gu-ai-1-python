129e906cbaa8df79a40356edcd1ec4a65dd5b0d2
asofronov <ynxela@gmail.com>
2020-08-01 14:19:13 +0300

    lesson_7

---
+++ lesson_7/2.py
@@ -0,0 +1,58 @@
+"""Реализовать проект расчета суммарного расхода ткани на производство одежды.
+Основная сущность (класс) этого проекта — одежда, которая может иметь определенное название.
+К типам одежды в этом проекте относятся пальто и костюм.
+У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма).
+Это могут быть обычные числа: V и H, соответственно.
+
+Для определения расхода ткани по каждому типу одежды использовать формулы:
+для пальто (V/6.5 + 0.5), для костюма (2*H + 0.3).
+Проверить работу этих методов на реальных данных.
+
+Реализовать общий подсчет расхода ткани.
+Проверить на практике полученные на этом уроке знания:
+реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+
+    @abstractmethod
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+
+    @property
+    @abstractmethod
+    def tissue_consumption(self):
+        pass
+
+
+class Coat(Clothes):
+
+    def __init__(self, size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.size = size
+
+    @property
+    def tissue_consumption(self):
+        return round(self.quantity * (self.size / 6.5 + 0.5), 2)
+
+
+class Suit(Clothes):
+
+    def __init__(self, height, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.height = height
+
+    @property
+    def tissue_consumption(self):
+        return round(self.quantity * (2 * self.height + 0.3), 2)
+
+
+coat = Coat(name='Пальто', size=44, quantity=2)
+suit = Suit(name='Костюм', height=1.73, quantity=3)
+
+print(coat.tissue_consumption)
+print(suit.tissue_consumption)
